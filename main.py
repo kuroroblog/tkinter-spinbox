a6ba559f3902ee7b45844c0b27362f60db23a326
tocchimittsu <arimagedon21@gmail.com>
2021-07-06 15:36:50 +0900

    修正

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Application(tk.Frame):
 
     # spinboxに関する情報を格納する関数
@@ -14,18 +15,15 @@ class Application(tk.Frame):
         self.master.geometry("300x200")
 
         # Windowを親要素として、frame Widget(Frame)を作成する。
-        # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+        # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
         frame = tk.Frame(self.master)
 
-        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
         # frame Widget(Frame)を親要素として、spinbox Widgetを作成する。
-        # from_ : 入力できる数字の下限を設定する。初期値とされる。
-        # to : 入力できる数字の上限を設定する。
-        # command : 右に設定される↑↓ボタンがクリックされた場合に、実行する関数を設定する。今回はhelloWorld
-        self.spinbox = tk.Spinbox(frame, from_=100, to=200, command=self.helloWorld)
+        self.spinbox = tk.Spinbox(frame)
 
         # frame Widget(Frame)を親要素とした場合に、spinbox Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
@@ -34,6 +32,7 @@ class Application(tk.Frame):
     # hello worldを出力する関数
     def helloWorld(self):
         print('hello world')
+
 
 # Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
 if __name__ == "__main__":
